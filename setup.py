ed81c83908cf93a989598952f1f51fde7c9885e9
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2023-11-21 23:21:55 -0800

    "Project Finished" "The last commit"

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ def get_requirements(file_path:str) ->List[str]:
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n" , "") for req in requirements]
-        #requirements = [req.replace("\n", "") for req in file_obj.readlines()]
 
     if HYPEN_E_DOT in requirements:
         requirements.remove(HYPEN_E_DOT)
